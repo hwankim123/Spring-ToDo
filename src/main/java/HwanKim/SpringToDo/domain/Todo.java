32b3ff7f10968d45f8341974cbb76be9c020a88d
Your Name <hwankim123@naver.com>
2022-03-24 16:12:33 +0900

    Todo, TodoTask 엔티티 수정. TodoRepository 개발 필요

--- src/main/java/HwanKim/SpringToDo/domain/Todo.java
+++ src/main/java/HwanKim/SpringToDo/domain/Todo.java
@@ -24,4 +24,28 @@ public class Todo {
     private LocalDate todoDate;
     @Enumerated(EnumType.STRING)
     private TodoStatus status; // 오늘의 할일 상태 [READY, RUNNING, FINISH]
+
+    //===연관관계 메서드===//
+    public void addTodoTask(TodoTask todoTask){
+        this.todoTasks.add(todoTask);
+        todoTask.setTodo(this);
+    }
+
+    public static Todo create(Member member, TodoTask... todoTasks ){
+        Todo todo = new Todo();
+        todo.setMember(member);
+        for(TodoTask todoTask : todoTasks){
+            todo.addTodoTask(todoTask);
+        }
+        todo.setStatus(TodoStatus.READY);
+        return todo;
+    }
+
+    private void setStatus(TodoStatus todoStatus) {
+        this.status = todoStatus;
+    }
+
+    private void setMember(Member member) {
+        this.member = member;
+    }
 }
--- src/main/java/HwanKim/SpringToDo/domain/TodoTask.java
+++ src/main/java/HwanKim/SpringToDo/domain/TodoTask.java
@@ -25,4 +25,24 @@ public class TodoTask {
     @Column(columnDefinition = "TEXT")
     private String desc;
 
+    // 연관관계 메서드를 위한 setter
+    public void setTodo(Todo todo) {
+        this.todo = todo;
+    }
+
+    //===생성 메서드===//
+    public static TodoTask createTodoTask(Task task, Duration taskDuration){
+        TodoTask todoTask = new TodoTask();
+        todoTask.setTask(task);
+        todoTask.setTaskDuration(taskDuration);
+        return todoTask;
+    }
+
+    private void setTaskDuration(Duration taskDuration) {
+        this.taskDuration = taskDuration;
+    }
+
+    private void setTask(Task task) {
+        this.task = task;
+    }
 }
