66347f498544291386c934baeb36ac05fbf94298
hwankim123 <hwankim1941@gmail.com>
2022-09-21 19:29:09 +0900

    fix: sequence_table을 직접 create해준 후 user 엔티티의 GeneratedValue strategy만 IDENTITY로 변경

--- src/main/java/HwanKim/SpringToDo/domain/User.java
+++ src/main/java/HwanKim/SpringToDo/domain/User.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 public class User extends BaseTimeEntity{
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
 
